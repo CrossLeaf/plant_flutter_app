a6c9326e8f7954616b604119e54f08105405f6bb
eton <pk2p007@hotmail.com.tw>
2021-05-14 01:35:28 +0800

    add: search bar

--- lib/screen/home/components/body.dart
+++ lib/screen/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:plant_flutter_app/constants.dart';
 
 class Body extends StatelessWidget {
@@ -21,7 +22,43 @@ class Body extends StatelessWidget {
                     borderRadius: BorderRadius.only(
                         bottomLeft: Radius.circular(36),
                         bottomRight: Radius.circular(36))),
-              )
+              ),
+              Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                    padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                    height: 54,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              offset: Offset(0, 10),
+                              blurRadius: 50,
+                              color: kPrimaryColor.withOpacity(0.23))
+                        ]),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Search",
+                              hintStyle: TextStyle(
+                                  color: kPrimaryColor.withOpacity(0.5)),
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              // suffixIcon: SvgPicture.asset("assets/icons/search.svg"),
+                            ),
+                          ),
+                        ),
+                        SvgPicture.asset("assets/icons/search.svg"),
+                      ],
+                    ),
+                  ))
             ],
           ),
         ),
