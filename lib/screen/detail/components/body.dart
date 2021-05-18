fe776aba5532307dce33c3846c8a379a263e8121
eton <pk2p007@hotmail.com.tw>
2021-05-18 23:45:57 +0800

    add: detail - icon card

--- lib/screen/detail/components/body.dart
+++ lib/screen/detail/components/body.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_flutter_app/constants.dart';
 
+import 'icon_card.dart';
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,24 +11,35 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         SizedBox(
-          // height: size.height * 0.8,
+          height: size.height * 0.8,
           child: Row(
             children: [
               Expanded(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: kDefaultPadding * 3),
                   child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: IconButton(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                        icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        }),
-                  )
-                ],
-              )),
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: IconButton(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: kDefaultPadding),
+                            icon:
+                                SvgPicture.asset("assets/icons/back_arrow.svg"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            }),
+                      ),
+                      Spacer(),
+                      IconCard(icon: "assets/icons/sun.svg"),
+                      IconCard(icon: "assets/icons/icon_2.svg"),
+                      IconCard(icon: "assets/icons/icon_3.svg"),
+                      IconCard(icon: "assets/icons/icon_4.svg"),
+                    ],
+                  ),
+                ),
+              ),
               Container(
                 height: size.height * 0.8,
                 width: size.width * 0.75,
